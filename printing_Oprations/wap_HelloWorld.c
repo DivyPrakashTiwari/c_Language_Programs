29c9e021139990b11c6b6fc6c174df67e65473e1
DivyPrakashTiwari <divyprakashtiwari.2024@gmail.com>
2024-04-24 08:03:53 +0530

    first commit to printing_Oprations

---
+++ printing_Oprations/wap_HelloWorld.c
@@ -0,0 +1,18 @@
+// Include the standard input/output library
+// This library contains the functions used for printing output, such as printf()
+#include<stdio.h>
+
+// Define the main function, which is the entry point of the program
+// The main function is where the program's execution begins
+// The int keyword indicates that the function returns an integer value
+int main()
+{
+    // Print "HELLO WORLD" to the console
+    // This is a call to the printf() function, which prints the string passed to it as an argument to the console
+    // In this case, it prints "HELLO WORLD" followed by a newline character (\n)
+    printf("HELLO WORLD !!\n");
+
+    // Return 0 to indicate that the program has executed successfully
+    // This line indicates that the function has executed successfully and returns 0 to the operating system
+    return 0;
+}
---
+++ printing_Oprations/wap_Printing_Opration.c
@@ -0,0 +1,37 @@
+#include<stdio.h> 
+// This is a preprocessor command that includes the standard input/output library in the code.
+
+int main() 
+// This is the main function where the program starts execution.
+{
+    // Print name
+    printf("NAME : DIVY PRAKASH TIWARI\n"); 
+    // This line prints out the name "DIVY PRAKASH TIWARI" followed by a newline character (\n).
+
+    // Print email
+    printf("E MAIL : divyprakashtiwari.2024@gmail.com\n"); 
+    // This line prints out the email address "divyprakashtiwari.2024@gmail.com" followed by a newline character (\n).
+
+    // Print address
+    printf("ADDRESS : JAMSHEDPUR ; JHARKHAND ; INDIA ; EARTH ; MILKYWAY GALAXY\n"); 
+    // This line prints out the address, which includes the city, state, country, planet, and galaxy, followed by a newline character (\n).
+
+    // Print education
+    printf("EDUCATION : B.Tech [CSE]\n"); 
+    // This line prints out the education details, which is a Bachelor of Technology (B.Tech) degree in Computer Science and Engineering (CSE), followed by a newline character (\n).
+
+    // Print university
+    printf("C.V. RAMAN GLOBAL UNIVERSITY, BHUBANESWAR\n"); 
+    // This line prints out the name of the university, "C.V. RAMAN GLOBAL UNIVERSITY", followed by a newline character (\n).
+
+    // Print experience
+    printf("EXPERIENCE : Management Lead at Gooogle Developers Student CLub CGU\n"); 
+    // This line prints out the work experience, which is a Management Lead position at the Google Developers Student Club at C.V. Raman Global University, followed by a newline character (\n).
+
+    // Print skills
+    printf("SKILLS : Cloud computing ; Content writing \n"); 
+    // This line prints out the skills, which are Cloud computing and Content writing, followed by a newline character (\n).
+
+    return 0; 
+    // This line indicates that the program has executed successfully.
+}

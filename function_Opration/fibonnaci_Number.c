3e6e6a08f9de7670fcb4463ccb84fa5fb6b62388
Divy Prakash Tiwari <divyprakashtiwari.2024@gmail.com>
2024-05-01 04:39:55 +0530

    Update fibonnaci_Number.c

--- function_Opration/fibonnaci_Number.c
+++ function_Opration/fibonnaci_Number.c
@@ -2,45 +2,8 @@
 
 int main()
 {
-    // Variable to store the user's choice
-    int e;
-
-    // Display the menu to the user
-    printf("welcome to the menu driven program\n");
-    printf("1.Program for factorial\n");
-    printf("2.program for fibonaaci\n");
-    printf("enter the program you want to access\n");
-
-    // Read the user's choice
-    scanf("%d",&e);
-
-    // Switch case to handle different user choices
-    switch(e)	
-    {
-        case 1:
-        {
-            // Variables for factorial calculation
-            int n, i, fac;
-
-            // Initialize factorial to 1
-            fac = 1;
-
-            // Prompt the user to enter a number
-            printf("Enter the number who's factorial to be calculated");
-            scanf("%d",&n);
-
-            // Calculate the factorial
-            for (i = n; i > 0; i--)
-            {
-                fac = fac * i;
-            }
-
-            // Display the result
-            printf("The factorial of %d is =%d", n, fac);		
-            break;
-        }
-        case 2:
-        {
+    printf("Program for fibonaaci\n");
+            
             // Variables for Fibonacci sequence
             int n, fib, i;
 
@@ -62,13 +25,6 @@ int main()
                 i++;
                 printf("%d ", fib);
             }
-            break;
-        }
-        default :
-        {
-            // Handle invalid user input
-            printf("the enter choice is wrong");
-        }
-    }	
+    
     return 0;
 }
